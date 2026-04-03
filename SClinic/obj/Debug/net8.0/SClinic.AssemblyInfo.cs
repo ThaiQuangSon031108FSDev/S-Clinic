165bfc5903e64e8d95835b0fe34a836004c08f58
ThaiQuangSon031108FSDev <sontqtd001282@gmail.com>
2026-04-03 09:51:10 +0700

    test

--- SClinic/obj/Debug/net8.0/SClinic.AssemblyInfo.cs
+++ SClinic/obj/Debug/net8.0/SClinic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SClinic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a67bbceb1eab72c6a9366d67e4b86e331e7cd8ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eae32da7798c684cf5dab35dfbf4247f278d472")]
 [assembly: System.Reflection.AssemblyProductAttribute("SClinic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SClinic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
